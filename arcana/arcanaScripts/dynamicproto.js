54e621a8429320b41cc02e53976f67d690d9d2ed
Carlos Rodriguez <element.cr@gmail.com>
2018-04-16 17:37:26 -0400

    Dynamic data changes finished. First successful dynm scrape.

--- arcana/arcanaScripts/dynamicproto.js
+++ arcana/arcanaScripts/dynamicproto.js
@@ -9,25 +9,21 @@ var procArray = new Array();
 let results = new Array();
 let promiseArray = new Array();
 
-var readfiledirectory = __dirname + '/../rawdata/'+ dateString + '.txt';
-var writefiledirectory = __dirname + '/../constants/' + dateString + '_const.json'
-
-console.log('>>>>>', readfiledirectory);
-//console.log(writefiledirectory);
+var readfiledirectory = __dirname + '/../rawdata/' + dateString + '.txt';
+var writefiledirectory = __dirname + '/../dynamic/' + dateString + '_dynamic.json'
 
 let ws = fs.createWriteStream(writefiledirectory);
 
 bundle(readfiledirectory);
 
 function bundle(readfiledirectory) {
-    console.log(readfiledirectory, ' >>>>>>>>>>In the Bundle Context');
     createArray(readfiledirectory)
         .then(async data => {
-            subData = data.splice(0,10);
-            processBatch(subData, 10, procArray).then((processed)=>{
-                for(let i = 0; i < procArray.length; i++){
-                    for(let j = 0; j < procArray[i].length; j++){
-                       results.push(procArray[i][j]);
+            subData = data.splice(0, data.length);
+            processBatch(subData, 10, procArray).then((processed) => {
+                for (let i = 0; i < procArray.length; i++) {
+                    for (let j = 0; j < procArray[i].length; j++) {
+                        results.push(procArray[i][j]);
                     }
                 }
                 console.log(results);
@@ -37,9 +33,9 @@ function bundle(readfiledirectory) {
         })
 }
 
-function processBatch(masterList, batchSize, procArray){
+function processBatch(masterList, batchSize, procArray) {
     return Promise.all(masterList.splice(0, batchSize).map(async url => {
-        return singleScrape(url) //.then(listing => console.log(listing));
+        return singleScrape(url)
     })).then((results) => {
         if (masterList.length < batchSize) {
             console.log('done');
@@ -54,7 +50,7 @@ function processBatch(masterList, batchSize, procArray){
 }
 
 async function singleScrape(url) {
-    let bbyid = url.substring(url.indexOf('=')+1);
+    let bbyid = url.substring(url.indexOf('=') + 1);
     let browser = await puppeteer.launch({
         headless: true
     });
@@ -64,42 +60,103 @@ async function singleScrape(url) {
     });
 
     await page.waitFor(1000);
-    let result = await page.evaluate(() => {
+
+    let result1 = await page.evaluate(() => {
         let appTitle = document.querySelector('.appx-page-header-2_title');
         appTitle = appTitle ? appTitle.innerText : '';
+
         let companyName = document.querySelector('.appx-company-name');
         companyName = companyName ? companyName.innerText : '';
-        let dateListed = document.querySelector('.appx-detail-section-first-listed p:nth-child(2)');
-        dateListed = dateListed ? dateListed.innerText : '';
-        let category = document.querySelector('.appx-detail-section:nth-child(3) a strong');
-        category = category ? category.innerText : '';
-        let domain =  document.querySelector('div.appx-extended-detail-subsection-description.slds-truncate > a');
-        domain = domain ? domain.innerText : '';
 
+        let lastUpdate = document.querySelector('.appx-detail-section-last-update p:nth-child(2)');
+        lastUpdate = lastUpdate ? lastUpdate.innerText : '';
         return {
             appTitle,
             companyName,
-            dateListed,
-            category,
-            domain
+            lastUpdate,
+        }
+    });
+
+    await Promise.all([
+        // page.evaluate(() => {
+        //     var button = document.getElementById('tab-default-2__item');
+        // }),
+        page.click("a[id=tab-default-2__item]"),
+        page.waitFor(15000)
+    ]);
+
+    let result2 = await page.evaluate(() => {
+        let reviewPercentage = document.querySelectorAll('.appx-extended-detail-review-percentage');
+        console.log("After the review percentages.")
+
+        let percentages = {
+            five: '',
+            four: '',
+            three: '',
+            two: '',
+            one: ''
+        }
+
+        let price = document.querySelector('.appx-pricing-detail-header');
+        price = price ? price.innerText : '';
+
+        let reviewscount_5 = document.getElementsByClassName("appx-extended-detail-review-bar appx-review-bar-graph-horizontal-5");
+        let reviewscount_4 = document.getElementsByClassName("appx-extended-detail-review-bar appx-review-bar-graph-horizontal-4");
+        let reviewscount_3 = document.getElementsByClassName("appx-extended-detail-review-bar appx-review-bar-graph-horizontal-3");
+        let reviewscount_2 = document.getElementsByClassName("appx-extended-detail-review-bar appx-review-bar-graph-horizontal-2");
+        let reviewscount_1 = document.getElementsByClassName("appx-extended-detail-review-bar appx-review-bar-graph-horizontal-1");
+
+        let actuals = {
+            five: '',
+            four: '',
+            three: '',
+            two: '',
+            one: ''
+        }
+
+        percentages.five = reviewPercentage[0] ? reviewPercentage[0].innerText : '';
+        percentages.four = reviewPercentage[1] ? reviewPercentage[1].innerText : '';
+        percentages.three = reviewPercentage[2] ? reviewPercentage[2].innerText : '';
+        percentages.two = reviewPercentage[3] ? reviewPercentage[3].innerText : '';
+        percentages.one = reviewPercentage[4] ? reviewPercentage[4].innerText : '';
+
+        actuals.five = reviewscount_5[0] ? reviewscount_5[0].dataset.reviewCount : '';
+        actuals.four = reviewscount_4[0] ? reviewscount_4[0].dataset.reviewCount : '';
+        actuals.three = reviewscount_3[0] ? reviewscount_3[0].dataset.reviewCount : '';
+        actuals.two = reviewscount_2[0] ? reviewscount_2[0].dataset.reviewCount : '';
+        actuals.one = reviewscount_1[0] ? reviewscount_1[0].dataset.reviewCount : '';
+
+        let totalReviews = reviewscount_5[0] ? reviewscount_5[0].dataset.reviewTotal : '';
+
+        let overallRating = document.querySelector('.appx-average-rating-numeral');
+        overallRating = overallRating ? overallRating.innerText : '';
+
+        return {
+            price,
+            percentages,
+            actuals,
+            totalReviews,
+            overallRating
         }
     });
 
     let urlData = {
         id: bbyid,
-        appName: result.appTitle,
-        companyName: result.companyName,
-        dateListed: result.dateListed,
-        category: result.category,
-        domain: result.domain
+        appName: result1.appTitle,
+        companyName: result1.companyName,
+        lastUpdate: result1.lastUpdate,
+        price: result2.price,
+        percentages: result2.percentages,
+        actuals: result2.actuals,
+        totalReviews: result2.totalReviews,
+        overallRating: result2.overallRating
     }
     await browser.close();
     return urlData;
 }
 
-function createArray(readfiledirectory){
+function createArray(readfiledirectory) {
     return new Promise((resolve, reject) => {
-        console.log(readfiledirectory, ' In the creat Array Context');
         fs.readFile(readfiledirectory, 'utf8', function (err, contents) {
             if (err) {
                 reject(err)
---
+++ arcana/mongoScripts/insert_dynamics.js
@@ -0,0 +1,43 @@
+var MongoClient = require('mongodb').MongoClient;
+var fs = require('fs');
+
+var url = "mongodb://localhost:27017/";
+
+var d1 = new Date();
+var dateString = (d1.getMonth() + '_' + d1.getDate() + '_' + d1.getFullYear());
+var constArr = new Array();
+
+function createArray() {
+    return new Promise((resolve, reject) => {
+        fs.readFile(__dirname + '/../dynamic/'+ dateString + '_dynamic.json', 'utf8', function (err, contents) {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(constArr = JSON.parse(contents))
+            }
+        })
+    })
+}
+
+createArray().then(data => {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("arcana");
+        dbo.collection("dynamics").insertMany(data, function(err, res) {
+          if (err) throw err;
+          console.log("Number of documents inserted: " + res.insertedCount);
+          db.close();
+        });
+      });
+})
+
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("arcana");
+
+//   dbo.collection("constants").insertMany(myobj, function(err, res) {
+//     if (err) throw err;
+//     console.log("Number of documents inserted: " + res.insertedCount);
+//     db.close();
+//   });
+// });
